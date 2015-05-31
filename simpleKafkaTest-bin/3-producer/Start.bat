d7bd8afa5b02361e378c1a07f1199e980a025a2a
zhuxiuwei <jlu_zxw@126.com>
2015-05-31 18:57:10 +0800

    update server info

--- simpleKafkaTest-bin/3-producer/Start.bat
+++ simpleKafkaTest-bin/3-producer/Start.bat
@@ -1,2 +1,2 @@
 set path=%path%;%~dp0openjdk-1.7.0-u80-unofficial-windows-i586-image\bin
-java -jar SimpleProducer.jar localhost:9092
+java -jar SimpleProducer.jar CO3SCH010132607:9092
--- simpleKafkaTest-bin/4-consumer/Start.bat
+++ simpleKafkaTest-bin/4-consumer/Start.bat
@@ -1,2 +1,2 @@
 set path=%path%;%~dp0openjdk-1.7.0-u80-unofficial-windows-i586-image\bin
-java -jar SimpleHLComsumer.jar localhost:2181
+java -jar SimpleHLComsumer.jar CO3SCH010080328:2181
